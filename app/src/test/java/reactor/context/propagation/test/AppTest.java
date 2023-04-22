ee5fbd61596a3f7866cb9b0e71b9f0f91e70ac91
XodiumLuma <28709874+xodiumluma@users.noreply.github.com>
2023-04-23 00:33:15 +1000

    Submit threads to an executor service

--- app/src/test/java/reactor/context/propagation/test/AppTest.java
+++ app/src/test/java/reactor/context/propagation/test/AppTest.java
@@ -6,15 +6,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Hooks;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+import reactor.util.Loggers;
+import reactor.util.context.Context;
+
+import java.time.Duration;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
-
-import reactor.util.Loggers;
-import reactor.util.context.Context;
-import reactor.test.StepVerifier;
-
-import java.time.Duration;
 
 class AppTest {
   private ContextRegistry registry;
@@ -46,32 +48,54 @@ class AppTest {
     }
   }
 
-  @Test void worksBetweenThreads() {
+  @Test void worksBetweenThreads() throws InterruptedException {
+
+    class PrivateThread implements Runnable {
+      private static final ThreadLocal<String> MY_ID = new ThreadLocal<>();
+      private final String CONTEXT_KEY = "CONTEXT_KEY:";
+      private final String PREFIX = "HELLO-PREFIX-كيف حالك-";
+      private final String GREETING = "HI!";
+      private final String ITERATION_ID;
+
+      PrivateThread(String iterationId) {
+        this.ITERATION_ID = PREFIX + iterationId;
+      }
+
+      @Override public void run() {
+        registry.registerThreadLocalAccessor(CONTEXT_KEY + ITERATION_ID,
+          MY_ID::get,
+          MY_ID::set,
+          MY_ID::remove);
+        Hooks.enableAutomaticContextPropagation();
+        Mono<String> handler = Mono.defer(() -> Mono.just(GREETING)
+          .delayElement(Duration.ofSeconds(2))
+          .map(v -> v + MY_ID.get())
+          .log()
+          .contextWrite(Context.of(CONTEXT_KEY + ITERATION_ID, ITERATION_ID)));
+
+        assertNull(MY_ID.get());
+        final String BOO = "BOO!";
+        MY_ID.set(BOO);
+        assertEquals(MY_ID.get(), BOO);
+
+        StepVerifier.create(handler)
+          .expectNext(GREETING + ITERATION_ID)
+          .expectComplete()
+          .verify();
+
+        MY_ID.remove();
+      }
+    }
+
     Loggers.useConsoleLoggers();
-    final String CONTEXT_KEY = "كيف حالك";
-    final String ALPHANUMERIC_ID = "THIS IS MY ID!!!1234-567-ABC";
-    final String HELLO = "HELLO!";
-    registry.registerThreadLocalAccessor(CONTEXT_KEY,
-      MY_THREADLOCALSTORAGE::get,
-      MY_THREADLOCALSTORAGE::set,
-      MY_THREADLOCALSTORAGE::remove);
-    Hooks.enableAutomaticContextPropagation();
-    Mono<String> handler = Mono.defer(() -> Mono.just(HELLO)
-      .delayElement(Duration.ofSeconds(2))
-      .map(v -> v + MY_THREADLOCALSTORAGE.get())
-      .log()
-      .contextWrite(Context.of(CONTEXT_KEY, ALPHANUMERIC_ID)));
-
-    assertNull(MY_THREADLOCALSTORAGE.get());
-    final String BOO = "BOO!";
-    MY_THREADLOCALSTORAGE.set(BOO);
-    assertEquals(MY_THREADLOCALSTORAGE.get(), BOO);
-
-    StepVerifier.create(handler)
-      .expectNext(HELLO + ALPHANUMERIC_ID)
-      .expectComplete()
-      .verify();
-
-    MY_THREADLOCALSTORAGE.remove();
+    ExecutorService service = Executors.newCachedThreadPool();
+    final var MAX_TASKS = 50;
+    for (int i = 0; i < MAX_TASKS; i++) {
+      System.out.println("Iteration #: " + i);
+      PrivateThread privateThread = new PrivateThread("TASK ID: " + i);
+      service.submit(privateThread);
+      Thread.sleep(new Random().nextInt(1000));
+    }
+    service.shutdown();
   }
 }
